b895ea27d73d6f672fffeabf41b5ca564f0fb4ba
thanhloansqt <thanhloansqt@gmail.com>
2016-10-27 17:13:00 +0700

    Initialization

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'automation9',
+  packages = ['automation9'], # this must be the same as the name above
+  version = '0.1',
+  description = 'An automation library',
+  author = 'Nikola Dang',
+  author_email = 'ducthinhdt@gmail.com',
+  url = 'https://github.com/nikoladang/automation9', # use the URL to the github repo
+  download_url = 'https://github.com/nikoladang/automation9/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example', 'automation', 'signing'], # arbitrary keywords
+  classifiers = [],
+)
